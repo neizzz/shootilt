48ed29f4926b89bd07b0658da1ea987f80c837bf
ChoongHo <ch.neizzz@gmail.com>
2023-01-03 01:06:38 +0900

    chore: global GameContext, bullet assets setup

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// import '';
+
+// document.addEventListener('DOMContentLoaded', () => {
+window.GameContext = {
+  VIEW_WIDTH: window.innerWidth,
+  VIEW_HEIGHT: window.innerHeight,
+  MAX_ENTITY_COUNT: 1024,
+};
+// });
+
+// export {};
+
